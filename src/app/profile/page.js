05cf0d4e9d69552e0aaff36f79848a972ceb5c29
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-03-25 10:48:32 +0000

    profile page reviews done

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,49 +1,61 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import {auth, currentUser} from "@clerk/nextjs/server"
 import { db } from "../utils/db";
 import UserForm from "../components/UserForm";
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link";
 
 export default async function Page() {
-  const { userId, redirectToSignIn } = await auth();
-  const user = await currentUser();
 
-  const userInfo = await db.query(
-    `SELECT * FROM user_info WHERE clerk_id = $1`,
-    [userId]
-  );
+    const {userId, redirectToSignIn} = await auth()
 
-  if (!userId) return redirectToSignIn();
+    const user = await currentUser();
 
-  if (userInfo.rowCount == 0) {
-    console.log(user);
-    return (
-      <div>
-        <UserForm />
-      </div>
-    );
-  }
+    const userInfo = await db.query(`SELECT * FROM user_info WHERE clerk_id = $1`, [userId])
 
+    const clerkId = userInfo.rows[0].clerk_id
+
+    const data = await db.query(`SELECT * FROM game_review WHERE clerk_id = $1`, [clerkId])
+    const reviews = data.rows
+
+    const game = await db.query(`SELECT * FROM games WHERE id = $1`, [reviews[0].game_id])
+
+    console.log(reviews[0].game_id)
+
+    if(!userId) return redirectToSignIn()
+        
+        if(userInfo.rowCount ==0){
+            return(
+                <div>
+                    <UserForm />
+                </div>
+            )
+        }
+
+        async function renderReview(review) {
+            const game = await db.query("SELECT * FROM games WHERE id = $1", [
+              review.game_id,
+            ]);
+            return (
+              <div key={review.id}>
+                <p>
+                  <Link href={`/games/${review.game_id}`}>{game.rows[0].game_name}</Link>
+                </p>
+                <h2>{review.review_cont}</h2>
+              </div>
+            );
+          }
+        console.log(reviews[0].game_id)
   return (
     <>
-      <div className="m-5">
-        <p className="m-1">
-          Hello {userInfo.rows[0].username}, Welcome to your profile!
-        </p>
-        <Image
-          className="m-5 rounded-full shadow-black shadow-md"
-          src={user.externalAccounts[0].imageUrl}
-          height={200}
-          width={200}
-          alt="Your profile picture"
-        />
-        <p>What you told us about yourself: </p>
-
-        <p>{userInfo.rows[0].bio}</p>
-        <p>
-          Your account was created on:{" "}
-          {new Date(userInfo.rows[0].created_at).toLocaleDateString("en-GB")}
-        </p>
-      </div>
+        <div>
+            <p>Hello {userInfo.rows[0].username}, Welcome to your profile!</p>
+            <br/>
+            <Image className="m-5 rounded-full shadow-black shadow-md" src={user.externalAccounts[0].imageUrl} height={200} width={200} alt="Your profile picture"/>
+            <p>What you told us about yourself: </p>
+            <br/>
+            {reviews.map(renderReview)}
+            <p>{userInfo.rows[0].bio}</p>
+        </div>
     </>
   );
 }
