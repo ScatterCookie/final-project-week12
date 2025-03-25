215711a9f3775e2056a8a519610ef99e40e76b9f
Angus <angus.baker2468@gmail.com>
2025-03-25 10:20:32 +0000

    Added the stories page with links to the game the story is from

--- src/app/stories/page.js
+++ src/app/stories/page.js
@@ -1,10 +1,30 @@
+import Link from "next/link";
 import NavBar from "../components/NavBar";
+import { db } from "../utils/db";
 
-export default function Page() {
+export default async function Page() {
+  const stories = await db.query("SELECT * FROM game_stories");
+  async function renderStory(story) {
+    const game = await db.query("SELECT * FROM games WHERE id = $1", [
+      story.game_id,
+    ]);
+    return (
+      <div key={story.id}>
+        <h2>{story.story_title}</h2>
+        <p>{story.story_cont}</p>
+        <p>
+          This story is from:
+          <Link href={`/games/${story.game_id}`}>{game.rows[0].game_name}</Link>
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <NavBar />
-      <h1>Page</h1>
+      <h1>Stories Page</h1>
+      {stories.rows.map(renderStory)}
     </div>
   );
 }
