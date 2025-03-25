09f4d969a1c1599455d71e417e351c71b0e30d90
LiamCollins <avengedsevenfold.lc@gmail.com>
2025-03-25 10:23:08 +0000

    adding more maps

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -8,13 +8,30 @@ export default async function Page() {
     const {userId, redirectToSignIn} = await auth()
 
     const user = await currentUser();
-
+    
     const userInfo = await db.query(`SELECT * FROM user_info WHERE clerk_id = $1`, [userId])
+
+    const clerkId = userInfo.rows[0].clerk_id
+
+    const game = await db.query(`SELECT * FROM games WHERE id = $1`, [reviews[0].game_id])
+
+    console.log(reviews[0].game_id)
+
+    //  async function displayMap(){
+    //     'use server'
+    //     const data = await db.query(`SELECT * FROM game_review WHERE clerk_id = $1`, [clerkId])
+    //     const reviews = data.rows
+
+    //     reviews.map((review) => (
+    //         <div key={review.id}>
+    //             <p>{review.review_cont}</p>
+    //         </div>
+    //     ))
+    // }
 
     if(!userId) return redirectToSignIn()
         
         if(userInfo.rowCount ==0){
-            console.log(user)
             return(
                 <div>
                     <UserForm />
@@ -31,6 +48,9 @@ export default async function Page() {
             <p>What you told us about yourself: </p>
             <br/>
             <p>{userInfo.rows[0].bio}</p>
+        </div>
+        <div>
+            <p>{displayMap()}</p>
         </div>
     </>
   );
