18ed8b2c9e6038b41eba3a5a59f75f266a872b0e
eddy-sullivan <fleatoad@hotmail.com>
2025-03-26 13:24:38 +0000

    eddy17

--- src/app/components/NewGameForm.jsx
+++ src/app/components/NewGameForm.jsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/dist/server/api-utils";
 import { revalidatePath } from "next/cache";
 import { db } from "../utils/db";
+import newGameFormStyles from "../css/newGameForm.module.css";
 
 export default function NewGameForm() {
   async function handleNewGame(formData) {
@@ -17,35 +18,37 @@ export default function NewGameForm() {
     revalidatePath(`/games`);
   }
   return (
-    <form action={handleNewGame}>
-      <label htmlFor="game_name">Game Title</label>
-      <input
-        id="game_name"
-        name="game_name"
-        type="text"
-        placeholder="Game Title"
-        required
-      />
+    <div className={newGameFormStyles.container}>
+      <form action={handleNewGame}>
+        <label htmlFor="game_name">Game Title</label>
+        <input
+          id="game_name"
+          name="game_name"
+          type="text"
+          placeholder="Game Title"
+          required
+        />
 
-      <label htmlFor="game_info">Game Description</label>
-      <input
-        id="game_info"
-        name="game_info"
-        type="text"
-        placeholder="Description of Game"
-        required
-      />
+        <label htmlFor="game_info">Game Description</label>
+        <input
+          id="game_info"
+          name="game_info"
+          type="text"
+          placeholder="Description of Game"
+          required
+        />
 
-      <label htmlFor="game_img">Game Poster</label>
-      <input
-        id="game_img"
-        name="game_img"
-        type="text"
-        placeholder="Game Image URL"
-        required
-      />
+        <label htmlFor="game_img">Game Poster</label>
+        <input
+          id="game_img"
+          name="game_img"
+          type="text"
+          placeholder="Game Image URL"
+          required
+        />
 
-      <button type="submit">Submit Game</button>
-    </form>
+        <button type="submit">Submit Game</button>
+      </form>
+    </div>
   );
 }
